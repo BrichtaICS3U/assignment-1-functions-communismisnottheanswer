10dcf84e1b1cc79d142889585df504796a2f74e1
ALEXANDER HARRIS <S331394510@OCDSB>
2018-03-28 10:59:53 -0400

    finished step 2 and 3

--- main.py
+++ main.py
@@ -11,14 +11,42 @@
 #def FtoC ():
 #
 def CtoF(C):
-    """Input a temperature in Celicius to return a temperature in fahrenheit
+    """Input a temperature in Celicius to return a temperature in Fahrenheit
         C=Temperature in Celsius"""
     F = (1.8)*C+32
 
-    if C<-273:
+    if C<-273.15:
         return ("Invalid entry")
-    else:
-        return (F) 
 
-temperature = int(input('Enter your temperature in Celsius: '))
-print(CtoF(temperature))
+    else:
+        return (F)
+
+def FtoC(F):
+    """Input a temperature in Fahrenheit to return a temperature in Celsius
+        F=Temperature in Fahrenheit"""
+    C = (0.55556)*(F-32)
+
+    if F<-459.67:
+        return ("Invalid entry")
+
+    else:
+        return (C)
+
+
+
+
+
+pick = int(input('Enter 1 for Celsius or 2 for Fahrenheit: '))
+
+if pick == 1:
+
+    temperature = int(input('Enter your temperature in Celsius: '))
+    print(CtoF(temperature))
+
+elif pick == 2:
+
+    temperature = int(input('Enter your temperature in Fahrenheit: '))
+    print(FtoC(temperature))
+
+else:
+    print("learn how to count shithead")
