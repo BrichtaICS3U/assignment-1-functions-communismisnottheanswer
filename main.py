1cddb1994587deeff8138900f5f4ab1479df713e
ALEXANDER HARRIS <s331394510@ocdsb>
2018-03-28 10:39:21 -0400

    celsius to fahrenheit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Assignment 1
 # ICS3U
-# <your name>
+# <Alex Harris>
 # March 28, 2018
 
 ###### uncomment this when you are ready to work on it
@@ -10,6 +10,15 @@
 ###### uncomment this when you are ready to work on it
 #def FtoC ():
 #
+def CtoF(C):
+    """Input a temperature in Celicius to return a temperature in fahrenheit
+        C=Temperature in Celsius"""
+    F = (1.8)*C+32
+
+    if C<-273:
+        return ("Invalid entry")
+    else:
+        return (F) 
 
 temperature = int(input('Enter your temperature in Celsius: '))
-print(temperature)
+print(CtoF(temperature))
